313f040e1966506060a72369caf1deba6ea71059
Tyler Seward <tylerseward2341@gmail>
2021-10-29 21:26:18 -0500

    Create random and optimized play strategies, and implment an array randomizer.

--- 100_Prisoners/Program.cs
+++ 100_Prisoners/Program.cs
@@ -8,8 +8,41 @@ namespace _100_Prisoners
 {
     class Program
     {
+        static bool RandomPlay()
+        {
+            int[] cupboards = RandArr();
+
+
+        }
+
+        //static bool OptimizedPlay()
+        //{
+        //    int[] cupboards = RandArr();
+        //}
+
+        static int[] RandArr()
+        {
+            Random rng = new Random();
+            int[] cupboards = new int[100];
+
+            for (int i = 0; i < cupboards.Length; i++)
+            {
+                cupboards[i] = i + 1;
+            }
+
+            for (int i = cupboards.Length-1; i > 0; i--)
+            {
+                int j = rng.Next(0, i + 1);
+                int temp = cupboards[i];
+                cupboards[i] = cupboards[j];
+                cupboards[j] = temp;
+            }
+            return cupboards;
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
