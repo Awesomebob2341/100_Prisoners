e477c2a5856959673d93ba5104f33119f17dc57e
Tyler Seward <tylerseward2341@gmail>
2021-11-07 00:27:38 -0500

    Add Prisoners class and logic for checking random strat following prisoners..

---
+++ 100_Prisoners/Prisoner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_Prisoners
+{
+    class Prisoner
+    {
+        private List<int> cupboardsChecked = new List<int>();
+        public int id { get; }
+
+        public Prisoner(int id)
+        {
+            this.id = id;
+        }
+
+        public void AddCupboard(int cupboard)
+        {
+            cupboardsChecked.Add(cupboard);
+        }
+
+        public bool AlreadyChecked(int n)
+        {
+            if (cupboardsChecked.Contains(n))
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- 100_Prisoners/Program.cs
+++ 100_Prisoners/Program.cs
@@ -10,9 +10,32 @@ namespace _100_Prisoners
     {
         static bool RandomPlay()
         {
+            List<Prisoner> pass = new List<Prisoner>();
+            List<Prisoner> prisoners = CreatePrisoners();
+
             int[] cupboards = RandArr();
-
-
+            Random rand = new Random();
+            
+            for (int i = 0; i < prisoners.Count; i++)
+            {
+                for (int j = 0; j < 50; i++)
+                {
+                    int cupboard = rand.Next(cupboards.Length);
+                    
+                    if (!prisoners[i].AlreadyChecked(cupboard))
+                    {
+                        if (cupboards[j] == prisoners[i].id)
+                        {
+                            pass.Add(prisoners[i]);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         //static bool OptimizedPlay()
@@ -20,6 +43,18 @@ namespace _100_Prisoners
         //    int[] cupboards = RandArr();
         //}
 
+        static List<Prisoner> CreatePrisoners()
+        {
+            List<Prisoner> prisoners = new List<Prisoner>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Prisoner prisoner = new Prisoner(i);
+                prisoners.Add(prisoner);
+            }
+            return prisoners;
+        }
+        
         static int[] RandArr()
         {
             Random rng = new Random();
@@ -27,10 +62,10 @@ namespace _100_Prisoners
 
             for (int i = 0; i < cupboards.Length; i++)
             {
-                cupboards[i] = i + 1;
+                cupboards[i] = i;
             }
 
-            for (int i = cupboards.Length-1; i > 0; i--)
+            for (int i = cupboards.Length - 1; i > 0; i--)
             {
                 int j = rng.Next(0, i + 1);
                 int temp = cupboards[i];
